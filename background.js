840ed2f4b43c149788b3d376b5058678b72de545
mblason <marina.blasongraviz@gmail.com>
2022-06-18 12:05:25 +0200

    background done

--- background.js
+++ background.js
@@ -1,13 +1,36 @@
 class Background {
   constructor(ctx) {
-    this.img.src = "http://www.vbforums.com/attachment.php?attachmentid=113103&d=1398112502"
+    this.ctx = ctx;
+    this.img = new Image ();
+    this.img.src = "http://www.vbforums.com/attachment.php?attachmentid=113103&d=1398112502";
+    this.x = 0;
+    this.y = 0;
+    this.w = this.ctx.canvas.width;
+    this.h = this.ctx.canvas.height;
+    this.vx = 2;
   }
 
   draw() {
+    this.ctx.drawImage(
+      this.img,
+      this.x,
+      this.y,
+      this.w,
+      this.h,
+    )
 
+     this.ctx.drawImage(
+      this.img,
+      this.x + this.ctx.canvas.width,
+      this.y,
+      this.w,
+      this.h,
+    )
   }
 
   move() {
+    this.x -= this.vx;
 
+    if(this.x + this.w <= 0){this.x = 0};
   }
 }
--- game.js
+++ game.js
@@ -1,9 +1,22 @@
 class Game {
   constructor(ctx) {
+    this.ctx = ctx;
+    this.intervalId = null;
+    this.background = new Background(this.ctx);
+
+
   }
 
   start() {
-  }
+    this.intervalId = setInterval( () => {
+      this.clear();
+      this.draw();
+      this.move();
+      
+
+    }    
+    , 1000/60)
+    }
 
   clearObstacles() {
   }
@@ -12,12 +25,22 @@ class Game {
   }
 
   clear() {
+    this.ctx.clearRect(
+      0,
+      0,
+      this.ctx.canvas.width,
+      this.ctx.canvas.heigth
+    )
   }
 
   draw() {
+    this.background.draw();
+    
+
   }
   
   move() {
+    this.background.move();
   }
 
   checkCollisions() {
